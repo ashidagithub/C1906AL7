5ca9047114b484238ea2fa202d4307229554c9a7
ashidagithub <1019726435@qq.com>
2019-07-11 21:03:06 +0800

    changed to package

--- MyModules.py
+++ MyModules.py
@@ -7,37 +7,42 @@
 # 掌握函数的用法
 
 # 练习一：第一个函数
-print('\n---华丽分割线---练习（1）---')
+
+
 def hello():
+    'Desc: Print hello world by function'
     print("Function-Hello World!")
     return
 
-# 调用
-hello()
-
 # 练习二：打印指定字符串 - 传入字符串
-print('\n---华丽分割线---练习（2）---')
-def printme( str ):
-   # 打印任何传入的字符串
-   print (str)
-   return
 
-# 调用
-printme("xxxxx")
+
+def printme(str):
+    'Desc: Print a specified string'
+    # 打印任何传入的字符串
+    print(str)
+    return
 
 # 练习三：计算面积函数 - 传入数字
-print('\n---华丽分割线---练习（3）---')
+
+
 def area(width, height):
+    'Desc: Calculate area'
     return width * height
 
+
+'''
 # 调用
 w = 4
 h = 5
 print("width =", w, " height =", h, " area =", area(w, h))
+'''
 
 # 练习四：将求三角形面积的海伦公式函数化
-print('\n---华丽分割线---练习（4）---')
+
+
 def triangle_area(a, b, c):
+    'Desc: Calculate triangle''s area '
     # 依据海伦公式求任意三角形面积
     # 已知三角形三边a,b,c,半周长p,则S＝ √[p(p - a)(p - b)(p - c)] （海伦公式）（p=(a+b+c)/2）
     # 计算半周长
@@ -48,6 +53,9 @@ def triangle_area(a, b, c):
     area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
     return area
 
+
+'''
 # 调用
-s = triangle_area(3,4,5)
+s = triangle_area(3, 4, 5)
 print(s)
+'''
---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+# ------------------------(max to 80 columns)-----------------------------------
+# author by : （学员ID)
+# created:  2019.7.10
+
+# Description:
+# 本模块为主模块，主程序，引用其他的模块
+# ------------------------(max to 80 columns)-----------------------------------
+
+import MyModules
+
+
+MyModules.hello()
